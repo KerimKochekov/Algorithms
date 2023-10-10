7f742636bd6171c5ace99df2e3305eab2a4255d3
Kerim Kochekov <50640842+KerimKochekov@users.noreply.github.com>
2023-10-11 00:04:46 +0800

    Update ordered_set.cpp

--- ordered_set.cpp
+++ ordered_set.cpp
@@ -1,30 +1,12 @@
 #include "bits/stdc++.h"
 #include <ext/pb_ds/assoc_container.hpp>
-#define MAXN 100009
-#define INF 1000000007
-#define mp(x,y) make_pair(x,y)
-#define all(v) v.begin(),v.end()
-#define pb(x) push_back(x)
-#define wr cout<<"----------------"<<endl;
-#define ppb() pop_back()
-#define tr(ii,c) for(__typeof((c).begin()) ii=(c).begin();ii!=(c).end();ii++)
-#define ff first
-#define ss second
-#define my_little_dodge 46
-#define debug(x)  cerr<< #x <<" = "<< x<<endl;
-using namespace std;
 using namespace __gnu_pbds;
+using namespace std;
 
-typedef long long ll;
-typedef pair<int,int> PII;
 typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update>ordered_set;
-template<class T>bool umin(T& a,T b){if(a>b){a=b;return 1;}return 0;}
-template<class T>bool umax(T& a,T b){if(a<b){a=b;return 1;}return 0;}
-
 int main(){
-	ordered_set X;
-	//*X.find_by_order(position)
-	//X.order_of_key(k) number of values in set smaller than k
-    
-	return 0;
+	ordered_set s;
+	s.insert(5);
+	*s.find_by_order(0);
+	s.order_of_key(0); //number of values in set smaller than k
 }
