32e454d46c483a34789401541291001d37a83516
Kerim Kochekov <50640842+KerimKochekov@users.noreply.github.com>
2023-10-11 00:01:09 +0800

    Update SCC.cpp

--- SCC.cpp
+++ SCC.cpp
@@ -1,43 +1,29 @@
 #include "bits/stdc++.h"
-#define MAXN 1000009
-#define INF 1000000007
-#define mp(x,y) make_pair(x,y)
-#define all(v) v.begin(),v.end()
-#define pb(x) push_back(x)
-#define wr cout<<"----------------"<<endl;
-#define ppb() pop_back()
-#define tr(ii,c) for(__typeof((c).begin()) ii=(c).begin();ii!=(c).end();ii++)
-#define ff first
-#define ss second
-#define my_little_dodge 46
-#define debug(x)  cerr<< #x <<" = "<< x<<endl;
+#define mp(x, y) make_pair(x, y)
 using namespace std;
- 
-typedef long long ll;
-typedef pair<int,int> PII;
-template<class T>bool umin(T& a,T b){if(a>b){a=b;return 1;}return 0;}
-template<class T>bool umax(T& a,T b){if(a<b){a=b;return 1;}return 0;}
-int in[MAXN],fup[MAXN],tin[MAXN],TIM,S,comp[MAXN];
-stack<int>st;
-vector<int>adj[MAXN],group[MAXN];
+const int N = 1e5 + 5;
+int dp[N], vis[N], T, S, comp[N], in[N];
+stack<int> st;
+vector<int> adj[N];
 void dfs(int nd){
-	tin[nd]=fup[nd]=++TIM;
-	st.push(nd);in[nd]=1;
-	tr(it,adj[nd]){
-		if(!tin[*it]){
-			dfs(*it);
-			umin(fup[nd],fup[*it]);	
-		}
-		else if(in[*it])
-			umin(fup[nd],tin[*it]);
-	}
-	if(fup[nd]==tin[nd]){
-		int aux;S++;
-		do{
-			aux=st.top();st.pop();
-			comp[aux]=S;in[aux]=0;
-			group[S].pb(aux);
-		}while(aux!=nd);
-	}
+    vis[nd] = dp[nd] = ++T;
+    st.push(nd); in[nd] = 1;
+    for (auto to: adj[nd]){
+        if (!vis[to]){
+            dfs(to);
+            dp[nd] = min(dp[nd], dp[to]);
+        }
+        else if (in[to])
+            dp[nd] = min(dp[nd], vis[to]);
+    }
+    if (dp[nd] == vis[nd]){
+        int aux;
+        do{
+            aux = st.top();
+            st.pop();
+			in[aux] = 0;
+            comp[aux] = S;
+        }while (aux != nd);
+        S += 1;
+    }
 }
-
